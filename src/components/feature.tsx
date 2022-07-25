abeef990b999afad40bdeaad7a41031980d148c8
unsignd <unsigndid@gmail.com>
2022-07-25 23:01:31 +0900

    add fade scroll animation

---
+++ src/components/feature.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import './styles/feature.css';
+
+export default function Feature() {
+  const [scrollProgress, setScrollProgress] = useState(0); // For scroll fade anim
+
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      if (window.scrollY < 1000) {
+        setScrollProgress(0);
+      } else {
+        setScrollProgress(((window.scrollY - 1000) / 1000) * 100);
+      }
+    });
+  }, []);
+
+  return (
+    <div className="Feature">
+      <div
+        className="animation"
+        style={{
+          top: '0vh',
+          width: `${Math.pow(scrollProgress, 1.25)}%`,
+          opacity: `${Math.pow(scrollProgress, 1.15) / 100}`,
+        }}
+      />
+      <div
+        className="animation"
+        style={{
+          top: '50vh',
+          width: `${Math.pow(scrollProgress, 1.25)}%`,
+          opacity: `${Math.pow(scrollProgress, 1.15) / 100}`,
+        }}
+      />
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './components/header';
 import Introduce from './components/introduce';
+import Feature from './components/feature';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,5 +12,6 @@ root.render(
   <React.StrictMode>
     <Header />
     <Introduce />
+    <Feature />
   </React.StrictMode>
 );
