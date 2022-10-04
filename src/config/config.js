858da627c39139b033a21d0602971e5879ef11d3
unsignd <unsigndid@gmail.com>
2022-10-04 18:43:56 +0900

    use .env

---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+import process from 'process';
+
+export const googleClientID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+export const kakaoRestAPIKey = process.env.REACT_APP_KAKAO_REST_API_KEY;
+export const workspaceURL = 'https://workspace.selenod.com';
+export const serverURL = 'https://api.selenod.com';
