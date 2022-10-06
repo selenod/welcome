09a2f2a77f154e18721c0083069a53e05d29e101
unsignd <unsigndid@gmail.com>
2022-10-06 14:06:16 +0900

    setup local env

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,9 @@ import process from 'process';
 
 export const googleClientID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 export const kakaoRestAPIKey = process.env.REACT_APP_KAKAO_REST_API_KEY;
-export const workspaceURL = 'https://workspace.selenod.com';
-export const serverURL = 'https://api.selenod.com';
+export const workspaceURL = process.env.REACT_APP_BUILD
+  ? 'https://workspace.selenod.com'
+  : 'http://localhost:3002';
+export const serverURL = process.env.REACT_APP_BUILD
+  ? 'https://api.selenod.com'
+  : 'http://localhost:80';
